c4f85672c7c7ef832d708c9f79bc9f5e4deefb61
Yosvel Quintero <yosvelquintero@gmail.com>
2023-07-26 15:38:07 +0700

    fix: added exports

--- packages/modules/ui/src/lib/components/pages/index.ts
+++ packages/modules/ui/src/lib/components/pages/index.ts
@@ -1,5 +1,7 @@
+export * from './authn/page-activation/page-activation.component';
 export * from './authn/page-password-recovery/page-password-recovery.component';
 export * from './authn/page-sign-in/page-sign-in.component';
+export * from './authn/page-sign-out/page-sign-out.component';
 export * from './authn/page-sign-up/page-sign-up.component';
 export * from './page-home/page-home.component';
 export * from './page-layout/page-layout.component';
