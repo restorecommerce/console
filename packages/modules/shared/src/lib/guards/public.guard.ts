52682b27a33b9007e5ffc3384b2aa1b392d922f0
Yosvel Quintero <yosvelquintero@gmail.com>
2023-08-11 03:31:27 +0700

    chore: shared module refactor

---
+++ packages/modules/shared/src/lib/guards/public.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { ROUTER } from '@console-core/config';
+import { AuthnFacade } from '@console-core/state';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PublicGuard {
+  constructor(
+    private readonly router: Router,
+    private readonly authnFacade: AuthnFacade
+  ) {}
+
+  canActivate(_: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.authnFacade.isNotAuthenticated$.pipe(
+      tap((isNotAuthenticated) => {
+        if (!isNotAuthenticated) {
+          this.router.navigate([ROUTER.pages.main.children.home.link], {
+            replaceUrl: true,
+          });
+        }
+      })
+    );
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.canActivate(route);
+  }
+}
