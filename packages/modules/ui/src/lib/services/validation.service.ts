0fca3f4a7d6f84e810944001643bbbe5c630804c
Yosvel Quintero <yosvelquintero@gmail.com>
2023-08-14 17:43:59 +0700

    chore: ui validation service

---
+++ packages/modules/ui/src/lib/services/validation.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RCValidationService {
+  validatePasswordMatch(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const password = control.get('password');
+    const passwordConfirmation = control.get('passwordConfirmation');
+
+    if (password?.value !== passwordConfirmation?.value) {
+      return { passwordConfirmationMismatch: true };
+    }
+
+    return null;
+  }
+}
