a8f19eb579bc3f671084b4ead4049b1e97697a55
Yosvel Quintero <yosvelquintero@gmail.com>
2023-09-19 13:07:04 +0700

    chore: code refactor

--- packages/core/config/src/lib/constants/date.ts
+++ packages/core/config/src/lib/constants/date.ts
@@ -3,9 +3,9 @@ import { IDateConstant } from '@console-core/types';
 export const DATE: Readonly<IDateConstant> = {
   format: {
     date: 'dd/MM/yyyy',
-    date_time: 'dd/MM/yyyy HH:mm',
-    date_time_seconds: 'dd/MM/yyyy HH:mm:ss',
+    dateTime: 'dd/MM/yyyy HH:mm',
+    dateTimeSeconds: 'dd/MM/yyyy HH:mm:ss',
     time: 'HH:mm',
-    time_seconds: 'HH:mm:ss',
+    timeSeconds: 'HH:mm:ss',
   },
 };
