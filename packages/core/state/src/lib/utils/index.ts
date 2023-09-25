72d583a926c512b1b6c351dac2419d3e7c97d446
Yosvel Quintero <yosvelquintero@gmail.com>
2023-09-25 13:55:57 +0700

    chore: code refactor

--- packages/core/state/src/lib/utils/index.ts
+++ packages/core/state/src/lib/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './capitalize-first-letter';
 export * from './rxjs-filters';
-export * from './operation-status';
 export * from './state';
 export * from './user';
--- packages/core/types/src/lib/interfaces/regex.ts
+++ packages/core/types/src/lib/interfaces/regex.ts
@@ -2,12 +2,4 @@ export interface IRegexConstant {
   email: RegExp;
   name: RegExp;
   password: RegExp;
-  http: {
-    response: {
-      error: {
-        unknown: RegExp;
-        unauthorized: RegExp;
-      };
-    };
-  };
 }
