9794d57c67b6da408d44de2f3489208ca3ddd073
Yosvel Quintero <yosvelquintero@gmail.com>
2023-10-25 21:37:18 +0700

    chore: auth module

--- packages/modules/authn/src/lib/components/confirm-email/confirm-email.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-authn-confirm-email',
-  template: `<p>confirm email works!</p>`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ConfirmEmailComponent {}
