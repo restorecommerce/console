1d310cb0d6b18c765694c72fa875231be824efe5
Yosvel Quintero <yosvelquintero@gmail.com>
2024-04-02 22:01:49 +0700

    chore: Add AppHttpInterceptor class to handle HTTP requests

--- apps/restorecommerce/src/app/app.module.ts
+++ apps/restorecommerce/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, TitleStrategy } from '@angular/router';
 
-// import { VCLDateAdapterModule } from '@vcl/ng-vcl';
+import { VCLProgressBarModule } from '@vcl/ng-vcl';
 
 import { CoreGraphQLModule } from '@console-core/graphql';
-// import { ModulesUiBaseModule, ModulesUiModule } from '@console-modules/ui';
 
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { AppStateModule } from './app.state.module';
+import { AppHttpInterceptor } from './http-interceptor';
 import { RoutesTitleStrategyService } from './routes-title-strategy.service';
 
 @NgModule({
@@ -29,14 +29,17 @@ import { RoutesTitleStrategyService } from './routes-title-strategy.service';
       api: environment.urls.graphql,
     }),
     AppStateModule,
-    // ModulesUiBaseModule,
-    // ModulesUiModule.forRoot(),
-    // VCLDateAdapterModule.forRoot(),
+    VCLProgressBarModule,
   ],
   providers: [
     {
       provide: 'apiUrl',
       useValue: environment.urls.api,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppHttpInterceptor,
+      multi: true,
     },
     {
       provide: TitleStrategy,
