122c333b9072bca74782a5259c0c1a80c10a197c
Yosvel Quintero <yosvelquintero@gmail.com>
2024-04-02 22:02:09 +0700

    chore: Refactor app.component.ts to include a progress bar

--- apps/restorecommerce/src/app/app.component.ts
+++ apps/restorecommerce/src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LoadingBarService } from '@ngx-loading-bar/core';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: ` <router-outlet />`,
+  template: `
+    <ng-container *ngIf="vm$ | async as vm">
+      <vcl-progress-bar [value]="vm.value" />
+    </ng-container>
+    <router-outlet />
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly vm$ = combineLatest({
+    value: this.loadingBar.value$,
+  });
+
+  constructor(private readonly loadingBar: LoadingBarService) {}
+}
