8fac5a685e8db0122020c0fc81708b5b253b3600
Yosvel Quintero <yosvelquintero@gmail.com>
2024-04-05 13:55:28 +0700

    chore: Refactor app.component.ts to improve UI layout and add progress bar

--- apps/restorecommerce/src/app/app.component.ts
+++ apps/restorecommerce/src/app/app.component.ts
@@ -5,10 +5,15 @@ import { combineLatest } from 'rxjs';
 @Component({
   selector: 'app-root',
   template: `
-    <ng-container *ngIf="vm$ | async as vm">
-      <vcl-progress-bar [value]="vm.value" />
-    </ng-container>
-    <router-outlet />
+    <div class="app">
+      <ng-container *ngIf="vm$ | async as vm">
+        <vcl-progress-bar
+          [value]="vm.value"
+          class="progress-bar"
+        />
+      </ng-container>
+      <router-outlet />
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
