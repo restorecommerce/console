259197db00eb3d4a20cf3537f13d07f9e2235731
Yosvel Quintero <yosvelquintero@gmail.com>
2024-04-05 23:06:24 +0700

    chore: Refactor error handling service and add new API type

--- packages/core/state/src/lib/services/error-handling.service.ts
+++ packages/core/state/src/lib/services/error-handling.service.ts
@@ -7,10 +7,27 @@ import { TOperationStatus } from '@console-core/types';
 })
 export class ErrorHandlingService {
   public checkStatusAndThrow(status?: TOperationStatus): void {
-    if (status?.code === 403) {
-      throw new Error('access denied');
-    } else if (status?.code !== 200) {
-      throw new Error(status?.message || 'unknown error');
+    const errorMessages: { [key: number]: string } = {
+      400: 'bad request',
+      401: 'unauthorized',
+      403: 'access not allowed',
+      404: 'not found',
+      408: 'request timeout',
+      500: 'internal server error',
+    };
+
+    let errorMessage = '';
+
+    if (status?.code) {
+      if (status?.code in errorMessages) {
+        errorMessage = errorMessages[status?.code];
+      } else if (status.code !== 200) {
+        errorMessage = status.message || 'unknown error';
+      }
+    }
+
+    if (errorMessage) {
+      throw new Error(errorMessage);
     }
   }
 }
--- packages/core/state/src/lib/services/index.ts
+++ packages/core/state/src/lib/services/index.ts
@@ -2,3 +2,4 @@ export * from './identity';
 export * from './master-data';
 export * from './ordering';
 export * from './api.service';
+export * from './error-handling.service';
