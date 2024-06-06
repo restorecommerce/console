1c8d4ee3e13b4ca12044bb5cb43a720297191880
Yosvel Quintero <yosvelquintero@gmail.com>
2024-06-06 14:25:49 +0700

    chore: Update user interface to include user roles

--- packages/core/types/src/lib/interfaces/entities/user.ts
+++ packages/core/types/src/lib/interfaces/entities/user.ts
@@ -1,12 +1,19 @@
-import { IoRestorecommerceUserUser } from '@console-core/graphql';
+import {
+  IoRestorecommerceUserUser,
+  IoRestorecommerceUserUserRole,
+} from '@console-core/graphql';
 
 import { IMeta } from './meta';
 
 export interface IUser
   extends Omit<
-    IoRestorecommerceUserUser,
-    'id' | 'email' | 'name' | 'firstName' | 'lastName' | 'meta'
-  > {
+      IoRestorecommerceUserUser,
+      'id' | 'email' | 'name' | 'firstName' | 'lastName' | 'meta' | '__typename'
+    >,
+    Omit<
+      IoRestorecommerceUserUserRole,
+      'id' | 'email' | 'name' | 'firstName' | 'lastName' | 'meta' | '__typename'
+    > {
   id: string;
   email: string;
   name: string;
