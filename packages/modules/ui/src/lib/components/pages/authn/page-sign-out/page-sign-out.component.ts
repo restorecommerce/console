e2a9db99a386fac76a4090708c5ec496c4a23d61
Yosvel Quintero <yosvelquintero@gmail.com>
2024-06-28 12:12:53 +0700

    feat: Update UI component styles for authentication pages

--- packages/modules/ui/src/lib/components/pages/authn/page-sign-out/page-sign-out.component.ts
+++ packages/modules/ui/src/lib/components/pages/authn/page-sign-out/page-sign-out.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'rc-page-authn-sign-out',
-  template: ` <p>Signing out. Please wait...</p> `,
+  templateUrl: './page-sign-out.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RcPageSignOutComponent {}
