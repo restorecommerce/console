2ee9138a8eba2f2746b7e1bf03bb168ea25b9a92
Bello Babakolo <fleetbeekay@gmail.com>
2024-08-20 11:23:50 +0100

    chore: clean up unused code

--- packages/modules/product/src/lib/jss-forms/product-variant.jss-form.ts
+++ packages/modules/product/src/lib/jss-forms/product-variant.jss-form.ts
@@ -23,7 +23,41 @@ export const buildProductVariantSchema = (
 
       //   ],
       // },
-
+      {
+        name: 'offerings',
+        label: 'Offerings',
+        type: 'select',
+        // disabled: true,
+        defaultValue: '',
+        validators: [Validators.required],
+        params: {
+          placeholder: 'Select offering',
+          selectionMode: 'single',
+          clearable: false,
+          search: false,
+          options: [
+            {
+              label: 'Physical',
+              value: 'physical',
+            },
+            {
+              label: 'Virtual',
+              value: 'virtual',
+            },
+            {
+              label: 'Service',
+              value: 'service',
+            },
+          ],
+        },
+        hints: [
+          {
+            type: 'error',
+            error: 'required',
+            message: 'This field is required.',
+          },
+        ],
+      },
       {
         name: 'name',
         label: 'Name',
@@ -132,41 +166,73 @@ export const buildProductVariantSchema = (
           },
         ],
       },
-      // {
-      //   name: 'offerings',
-      //   label: 'Offerings',
-      //   type: 'select',
-      //   // disabled: true,
-      //   defaultValue: '',
-      //   validators: [Validators.required],
-      //   params: {
-      //     placeholder: 'Select offering',
-      //     selectionMode: 'single',
-      //     clearable: false,
-      //     search: false,
-      //     options: [
-      //       {
-      //         label: 'Physical',
-      //         value: 'physical',
-      //       },
-      //       {
-      //         label: 'Virtual',
-      //         value: 'virtual',
-      //       },
-      //       {
-      //         label: 'Service',
-      //         value: 'service'
-      //       }
-      //     ],
-      //   },
-      //   hints: [
-      //     {
-      //       type: 'error',
-      //       error: 'required',
-      //       message: 'This field is required.',
-      //     },
-      //   ],
-      // },
+      // Price ****
+      {
+        name: 'currencyId',
+        label: 'Currency',
+        type: 'select',
+        // disabled: true,
+        defaultValue: '',
+        validators: [Validators.required],
+        params: {
+          placeholder: 'Select currency',
+          selectionMode: 'single',
+          clearable: false,
+          search: false,
+          options: [
+            {
+              label: 'USD',
+              value: 'USD',
+            },
+            {
+              label: 'EURO',
+              value: 'EUR',
+            },
+          ],
+        },
+        hints: [
+          {
+            type: 'error',
+            error: 'required',
+            message: 'This field is required.',
+          },
+        ],
+      },
+      {
+        name: 'regularPrice',
+        label: 'Regular price',
+        type: 'input',
+        validators: [Validators.required],
+        defaultValue: '',
+        params: {},
+        hints: [
+          {
+            type: 'error',
+            error: 'required',
+            message: 'This field is required.',
+          },
+        ],
+      },
+      {
+        name: 'salePrice',
+        label: 'Sale price',
+        type: 'input',
+        validators: [Validators.required],
+        defaultValue: '',
+        params: {},
+        hints: [
+          {
+            type: 'error',
+            error: 'required',
+            message: 'This field is required.',
+          },
+        ],
+      },
+      {
+        type: 'checkbox',
+        name: 'sales',
+        label: 'On sales',
+      },
       {
         type: 'buttons',
         buttons: [
--- packages/modules/product/src/lib/modules-product.module.ts
+++ packages/modules/product/src/lib/modules-product.module.ts
@@ -3,13 +3,10 @@ import { RouterModule } from '@angular/router';
 
 import { ModulesUiModule } from '@console-modules/ui';
 
-import { ProductCreationFormComponent } from './components/forms/product-creation-form.component';
 import { ProductFileFormComponent } from './components/forms/product-files-form.component';
 import { ProductImageFormComponent } from './components/forms/product-image-form.component';
 import { ProductPackageFormComponent } from './components/forms/product-package-form.component';
-import { ProductPriceFormComponent } from './components/forms/product-price-form.component';
 import { ProductPropertyFormComponent } from './components/forms/product-property-form.component';
-import { ProductVariantFormComponent } from './components/forms/product-variant-form.component';
 import { ProductCreateComponent } from './components/product-create.component';
 import { ProductEditComponent } from './components/product-edit.component';
 import { ProductIndexComponent } from './components/product-index.component';
@@ -25,9 +22,6 @@ import { modulesProductRoutes } from './lib.routes';
     ProductCreateComponent,
     ProductEditComponent,
     ProductViewComponent,
-    ProductCreationFormComponent,
-    ProductVariantFormComponent,
-    ProductPriceFormComponent,
     ProductImageFormComponent,
     ProductPropertyFormComponent,
     ProductFileFormComponent,
