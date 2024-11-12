55bcc9a2c1bb5e1d837a0695bf4d78b1a9f1b01d
Bello Babakolo <fleetbeekay@gmail.com>
2024-11-12 11:37:34 +0100

    feat(products) remove base products variant display

--- packages/modules/ui/src/lib/components/molecules/product-variants/product-variants.component.ts
+++ packages/modules/ui/src/lib/components/molecules/product-variants/product-variants.component.ts
@@ -22,4 +22,12 @@ export class RcProductVariantsComponent {
     new EventEmitter<IIoRestorecommerceProductPhysicalVariant>();
 
   @Output() deleteVariant = new EventEmitter<string>();
+
+  get variantsWithoutBaseVariant() {
+    return this.variants.slice(1);
+  }
+
+  get actualVariantLength() {
+    return this.variants.length - 1;
+  }
 }
