b958de80d952d17d4eaf72382b2a323012b9f6bd
Bello Babakolo <fleetbeekay@gmail.com>
2025-02-18 17:16:24 +0100

    feat(shop): implement shop index template

--- packages/modules/management/src/lib/components/shop/shop.module.ts
+++ packages/modules/management/src/lib/components/shop/shop.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [ShopIndexComponent],
+  declarations: [ShopTemplateComponent, ShopIndexComponent],
   imports: [ModulesUiModule.forChild(), RouterModule.forChild(routes)],
 })
 export class ShopModule {}
