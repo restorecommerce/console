addfbc868e7006e66573692dbe06fb4c3dc38a33
Bello Babakolo <fleetbeekay@gmail.com>
2025-03-14 17:35:49 +0100

    feat(invoice): add more detail to invoice page

--- packages/modules/invoice/src/lib/components/invoice-view-details.component.ts
+++ packages/modules/invoice/src/lib/components/invoice-view-details.component.ts
@@ -5,10 +5,91 @@ import { IInvoice } from '@console-core/types';
 @Component({
   selector: 'app-module-invoice-view-details',
   template: `
-    <pre>
-      {{ invoice | json }}
-    </pre
+    <vcl-data-list
+      [noBorder]="true"
+      mode="none"
     >
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Invoice number</span>
+          <span>{{ invoice.invoiceNumber }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>From date</span>
+          <span>{{ invoice.fromDate || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>To date</span>
+          <span>{{ invoice.toDate || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Shop id</span>
+          <span>{{ invoice.shopId || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>User id</span>
+          <span>{{ invoice.userId || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Status</span>
+          <span>{{ invoice.withdrawn || invoice.sent }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Payment</span>
+          <span>{{ invoice.paymentState || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Customer order number</span>
+          <span>{{ invoice.customerOrderNumber || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Customer id</span>
+          <span>{{ invoice.customerId || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Customer id</span>
+          <span>{{ invoice.customerId || 'N/A' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      <div class="my-2 rc-lv-l-heading">Total</div>
+      @for (total of invoice.totalAmounts; track $index) {
+      <vcl-data-list-item>
+        <div class="flex row justify-content-between align-item-center">
+          <span>Total (net)</span>
+          <span>{{ total?.net }} {{ total?.currency?.symbol || '€' }}</span>
+        </div>
+      </vcl-data-list-item>
+
+      }
+    </vcl-data-list>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
