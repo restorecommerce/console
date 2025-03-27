f79be0469d450324b6981f89ee384dd677cc41e0
Bello Babakolo <fleetbeekay@gmail.com>
2025-03-27 15:34:57 +0100

    feat(product): display variant files

--- packages/modules/ui/src/lib/components/atoms/popover-action/popover-action.component.ts
+++ packages/modules/ui/src/lib/components/atoms/popover-action/popover-action.component.ts
@@ -1,15 +1,61 @@
 // popover-action.component.ts
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+} from '@angular/core';
+import { SubSink } from 'subsink';
+
+import { AlertService, AlertType, PopoverDirective } from '@vcl/ng-vcl';
+
+import { InputMaybe } from '@console-core/graphql';
 
 @Component({
   selector: 'rc-popover-action',
   templateUrl: './popover-action.component.html',
   standalone: false,
 })
-export class RcPopoverActionComponent<T extends { id: string }> {
+export class RcPopoverActionComponent<
+  T extends { id?: string | InputMaybe<string> }
+> {
+  private readonly subscriptions = new SubSink();
+
   @Input() item!: T;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @Input() target: any;
+  @Input() target!: HTMLDivElement;
+  @Input() itemLabel!: string;
   @Output() edit = new EventEmitter<T>();
   @Output() delete = new EventEmitter<string>();
+
+  @ViewChild(PopoverDirective, { static: true }) popover!: PopoverDirective;
+
+  constructor(private readonly alertService: AlertService) {}
+
+  closePopover(): void {
+    this.popover?.close();
+  }
+
+  openPopover(): void {
+    this.popover?.open();
+  }
+
+  onDeleteClicked(): void {
+    this.subscriptions.sink = this.alertService
+      .open({
+        text: `Are you sure you want to delete "${this.itemLabel}"?`,
+        type: AlertType.Question,
+        showCloseButton: true,
+        showCancelButton: true,
+        cancelButtonLabel: 'Cancel',
+        cancelButtonClass: 'transparent',
+        confirmButtonLabel: 'Yes, Delete',
+        confirmButtonClass: 'button',
+      })
+      .subscribe((result) => {
+        if (result.action === 'confirm') {
+          this.delete.emit(this.item?.id || '');
+        }
+      });
+  }
 }
