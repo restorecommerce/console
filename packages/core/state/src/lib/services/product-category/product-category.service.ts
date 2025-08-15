81f10e7ada4b53f76168b864fd2d11a08a850038
Bello Babakolo <fleetbeekay@gmail.com>
2025-08-15 15:48:58 +0100

    feat(category): create product categry product service

---
+++ packages/core/state/src/lib/services/product-category/product-category.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { ApolloQueryResult } from '@apollo/client';
+import { MutationResult } from 'apollo-angular';
+import { Observable } from 'rxjs';
+
+import {
+  IIoRestorecommerceManufacturerManufacturerList,
+  IIoRestorecommerceResourcebaseDeleteRequest,
+  IIoRestorecommerceResourcebaseReadRequest,
+  ProductCategoryDeleteGQL,
+  ProductCategoryDeleteMutation,
+  ProductCategoryMutateGQL,
+  ProductCategoryMutateMutation,
+  ProductCategoryReadGQL,
+  ProductCategoryReadQuery,
+} from '@console-core/graphql';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductCategoryService {
+  constructor(
+    private readonly priceGroupReadGQL: ProductCategoryReadGQL,
+    private readonly priceGroupMutationGQL: ProductCategoryMutateGQL,
+    private readonly priceGroupDeleteGQL: ProductCategoryDeleteGQL
+  ) {}
+
+  read(
+    payload: IIoRestorecommerceResourcebaseReadRequest
+  ): Observable<ApolloQueryResult<ProductCategoryReadQuery>> {
+    return this.priceGroupReadGQL.fetch({
+      input: payload,
+    });
+  }
+
+  mutate(
+    payload: IIoRestorecommerceManufacturerManufacturerList
+  ): Observable<MutationResult<ProductCategoryMutateMutation>> {
+    return this.priceGroupMutationGQL.mutate({
+      input: payload,
+    });
+  }
+
+  remove(
+    payload: IIoRestorecommerceResourcebaseDeleteRequest
+  ): Observable<MutationResult<ProductCategoryDeleteMutation>> {
+    return this.priceGroupDeleteGQL.mutate({
+      input: payload,
+    });
+  }
+}
