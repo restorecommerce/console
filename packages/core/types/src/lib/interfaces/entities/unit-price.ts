ed3e281dda73b6323d1b408dbe060e56605de043
Bello Babakolo <fleetbeekay@gmail.com>
2025-09-19 15:32:24 +0100

    feat(order): add more property to the order view model

---
+++ packages/core/types/src/lib/interfaces/entities/unit-price.ts
@@ -0,0 +1,20 @@
+import { IoRestorecommercePricePrice } from '@console-core/graphql';
+
+import { ICurrency } from './currency';
+
+export interface IUnitPrice
+  extends Omit<
+    IoRestorecommercePricePrice,
+    | 'currencyId'
+    | 'sale'
+    | 'regularPrice'
+    | 'salePrice'
+    | 'currency'
+    | '__typename'
+  > {
+  currencyId: string;
+  sale: boolean;
+  regularPrice: number;
+  salePrice: number;
+  currency: ICurrency;
+}
