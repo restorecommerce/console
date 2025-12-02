fc7d1ce0c052a3c01fb613f2488da5e11d1b28e6
Bello Babakolo <fleetbeekay@gmail.com>
2025-12-02 11:55:57 +0100

    chore: setup l10n

--- apps/restorecommerce/src/app/app.component.ts
+++ apps/restorecommerce/src/app/app.component.ts
@@ -1,21 +1,8 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  template: ` <button (click)="switch('en')">EN</button>
-    <button (click)="switch('de')">DE</button>
-
-    <h1>{{ 'rs-ui.header.welcome' | translate : { name } }}</h1>
-    <router-outlet />`,
+  template: ` <router-outlet />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
-export class AppComponent {
-  name = 'babakolo';
-
-  private translate = inject(TranslateService);
-
-  switch(lang: string) {
-    this.translate.use(lang);
-  }
-}
+export class AppComponent {}
