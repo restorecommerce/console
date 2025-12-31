9e462d0cbab18976e4ba9c139477ff69675cf409
Bello Babakolo <fleetbeekay@gmail.com>
2025-12-31 15:33:51 +0100

    feat(fulfilment): wire fulfilment view

--- packages/modules/fulfillment/src/lib/components/fulfillment-view.component.ts
+++ packages/modules/fulfillment/src/lib/components/fulfillment-view.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-module-fulfillment-view',
-  template: ` <h3>View</h3> `,
+  template: ` <h3 class="m-0">View</h3> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FulfillmentViewComponent {}
