ad6b46650f9f22d699aeefdf685e38710434edf2
Bello Babakolo <fleetbeekay@gmail.com>
2026-01-29 23:51:33 +0100

    feat(order): maintain one router in template

--- packages/modules/order/src/lib/order.routes.ts
+++ packages/modules/order/src/lib/order.routes.ts
@@ -4,7 +4,6 @@ import { ROUTER } from '@console-core/config';
 
 import { OrderEmptyStateComponent } from './ui/components/order-empty-state/order-empty.component';
 import { OrderTemplateComponent } from './ui/components/order-template/order-template.component';
-import { OrderListComponent } from './ui/pages/order-list/order-list.component';
 import { OrderViewComponent } from './ui/pages/order-view/order-view.component';
 
 export const modulesOrderRoutes: Route[] = [
@@ -16,18 +15,10 @@ export const modulesOrderRoutes: Route[] = [
       /** Sidebar: always show order list */
       {
         path: '',
-        outlet: 'sidebar',
-        component: OrderListComponent,
-        title: ROUTER.pages.main.children.orders.children.index.title,
-      },
-      {
-        path: '',
-        outlet: 'content',
         component: OrderEmptyStateComponent,
       },
       {
         path: ROUTER.pages.main.children.orders.children.view.path, // e.g. ':orderId'
-        outlet: 'content',
         component: OrderViewComponent,
         title: ROUTER.pages.main.children.orders.children.view.title,
       },
--- packages/modules/order/src/lib/ui/components/order-template/order-template.component.ts
+++ packages/modules/order/src/lib/ui/components/order-template/order-template.component.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RcResourcePageLayoutComponent } from '@console/rc-ui';
 
+import { OrderListComponent } from '../../pages/order-list/order-list.component';
+
 @Component({
   selector: 'app-module-order-template',
   templateUrl: './order-template.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RcResourcePageLayoutComponent, RouterOutlet],
+  imports: [RcResourcePageLayoutComponent, RouterOutlet, OrderListComponent],
 })
 export class OrderTemplateComponent {}
