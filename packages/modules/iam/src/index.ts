ca704b58909529ec7c2158629582af4a37c14c7f
Bello Babakolo <fleetbeekay@gmail.com>
2026-02-10 17:26:08 +0100

    choree(iam): add IAM package

---
+++ packages/modules/iam/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/iam/iam';
---
+++ packages/modules/iam/src/lib/iam/iam.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-iam',
+  imports: [],
+  templateUrl: './iam.html',
+  styleUrl: './iam.css',
+})
+export class Iam {}
