f9710b561865b7a1d991cf2a22c00501615b49bc
Bello Babakolo <fleetbeekay@gmail.com>
2026-02-23 08:41:13 +0100

    feat(iam-user): set user creation component

---
+++ packages/modules/iam-user/src/lib/ui/pages/iam-user-create/iam-user-create.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-iam-user-create',
+  templateUrl: './iam-user-create.component.html',
+  imports: [],
+})
+export class IAMUserCreateComponent {}
