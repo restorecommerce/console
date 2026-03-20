ba31e969d0d093ace537de8213a4a5dd7d498be7
Bello Babakolo <fleetbeekay@gmail.com>
2026-03-20 17:40:32 +0100

    feat(iam): add notification upon user creation

--- packages/modules/iam-user/src/lib/store/user-create/user-create.effects.ts
+++ packages/modules/iam-user/src/lib/store/user-create/user-create.effects.ts
@@ -1,23 +1,36 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { RcNotifierService } from '@console/rc-ui';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
+
+import { ROUTER } from '@console-core/config';
 
 import { UserRepository } from '../../data/user.repository';
 
 import * as UserCreationActions from './user-create.actions';
 
 export const loadUserCreationEffect = createEffect(
-  (actions$ = inject(Actions), userRepository = inject(UserRepository)) => {
+  (
+    actions$ = inject(Actions),
+    userRepository = inject(UserRepository),
+    notifier = inject(RcNotifierService)
+  ) => {
     return actions$.pipe(
       ofType(UserCreationActions.createUser),
       switchMap(({ command }) =>
         userRepository.createUser(command).pipe(
           map((res) => UserCreationActions.createUserSuccess({ id: res.id })),
-          catchError((error) =>
-            of(UserCreationActions.createUserFailure({ error: error.message }))
-          )
+          catchError((error) => {
+            notifier.error(
+              'Create failed',
+              error.message ?? 'Failed to create user'
+            );
+            return of(
+              UserCreationActions.createUserFailure({ error: error.message })
+            );
+          })
         )
       )
     );
@@ -26,11 +39,18 @@ export const loadUserCreationEffect = createEffect(
 );
 
 export const navigateAfterCreateEffect = createEffect(
-  (actions$ = inject(Actions), router = inject(Router)) => {
+  (
+    actions$ = inject(Actions),
+    router = inject(Router),
+    notifier = inject(RcNotifierService)
+  ) => {
     return actions$.pipe(
       ofType(UserCreationActions.createUserSuccess),
       tap(({ id }) => {
-        router.navigate(['/iam', id, 'view']);
+        notifier.success('User created successfully');
+        router.navigate(
+          ROUTER.pages.main.children.iam.children.view.getLink({ id })
+        );
       })
     );
   },
