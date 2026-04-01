033576c4e9c25b3d7d9018b3709ab8edd1c56dd6
Bello Babakolo <fleetbeekay@gmail.com>
2026-04-01 13:02:56 +0100

    fix: imports for role assignment

--- packages/modules/iam-user/src/lib/ui/pages/iam-user-view/iam-user-view.component.ts
+++ packages/modules/iam-user/src/lib/ui/pages/iam-user-view/iam-user-view.component.ts
@@ -18,8 +18,8 @@ import {
 import { ROUTER } from '@console-core/config';
 
 import { IamUserViewFacade } from '../../../store';
+import { IAMUserAccessAssignmentsTabComponent } from '../../components/iam-user-access-assignments-tab/iam-user-roles-tab.component';
 import { IAMUserOverviewTabComponent } from '../../components/iam-user-overview-tab/iam-user-overview-tab.component';
-import { IAMUserAccessAssignmentsTabComponent } from '../../components/iam-user-roles-tab/iam-user-roles-tab.component';
 import { IAMUserSessionsTabComponent } from '../../components/iam-user-sessions-tab/iam-user-sessions-tab.component';
 import { IAMUserStatusBadgeComponent } from '../../components/iam-user-status-badge/iam-user-status-badge.component';
 
