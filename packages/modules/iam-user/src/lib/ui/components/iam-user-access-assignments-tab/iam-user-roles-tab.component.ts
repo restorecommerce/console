8e03f15170125f01325a6c85de3c970e840f875b
Bello Babakolo <fleetbeekay@gmail.com>
2026-04-01 13:29:20 +0100

    fix(access-assignments): style system and assigned role counts

--- packages/modules/iam-user/src/lib/ui/components/iam-user-access-assignments-tab/iam-user-roles-tab.component.ts
+++ packages/modules/iam-user/src/lib/ui/components/iam-user-access-assignments-tab/iam-user-roles-tab.component.ts
@@ -1,5 +1,7 @@
 import { Component, computed, input } from '@angular/core';
 
+import { VCLTokenComponent } from '@vcl/ng-vcl';
+
 import { UserDetail } from '../../../models';
 import { IAMUserAssignedAccessTable } from '../iam-user-assigned-access-table/iam-user-assigned-access-table.component';
 import { IAMUserSystemAccessTable } from '../iam-user-system-access-table/iam-user-system-access-table.component';
@@ -7,7 +9,11 @@ import { IAMUserSystemAccessTable } from '../iam-user-system-access-table/iam-us
 @Component({
   selector: 'app-iam-user-access-assignments-tab',
   templateUrl: './iam-user-access-assignments-tab.component.html',
-  imports: [IAMUserAssignedAccessTable, IAMUserSystemAccessTable],
+  imports: [
+    IAMUserAssignedAccessTable,
+    IAMUserSystemAccessTable,
+    VCLTokenComponent,
+  ],
 })
 export class IAMUserAccessAssignmentsTabComponent {
   vm = input.required<UserDetail>();
